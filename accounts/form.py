2827f8f83c70259941db217ce04a58173319d6bf
PunMahesh <np03cs4a210055@heraldcollege.edu.np>
2024-02-12 02:33:04 +0545

    Login and Registation

---
+++ accounts/form.py
@@ -0,0 +1,97 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django.contrib.auth.forms import UserChangeForm
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget= forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "autocomplete": "new-password"
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "autocomplete": "new-password"
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    contact = forms.IntegerField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    address = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    )
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect)
+
+    dob = forms.DateField(label='Date of birth')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2','first_name','last_name','contact','address','gender','dob')
+
+
+class UserProfileForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        fields = ['first_name', 'last_name', 'email']
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
 from django.urls import path
 from accounts import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls import handler404
+from . import views
+from django.urls import re_path
+# handler404 = 'accounts.views.custom_404'
 
 urlpatterns = [
     path("login",views.login_view,name="login"),
-    path("registration",views.register_view,name="registration"),
+    path("registration",views.registration_view,name="registration"),
 ]
