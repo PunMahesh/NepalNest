030cb8db03ed0d238faf14d76aaf9c5dcfa86c7b
PunMahesh <np03cs4a210055@heraldcollege.edu.np>
2024-03-06 19:56:39 +0545

    changes on propert listing field

--- static/js/showpass.js
+++ static/js/showpass.js
@@ -1,9 +1,10 @@
-const showPassword = document.querySelector("#show Password");
-const passwordField = document.querySelector("#password")
+document.addEventListener("DOMContentLoaded", function () {
+  var showPassword = document.getElementById("show-icon");
+  var passwordField = document.querySelector("#password-input-field");
 
-showPassword.addEventListener("click", function(){
-    this.classList.toggle("fa-eye-slash");
-    const type = passwordField.getAttribute("type")
-    === "password" ? "text" : "password";
-    passwordField.setAttribute("type",type);
-})
+  showPassword.addEventListener("click", function () {
+    this.classList.toggle("view-hide");
+    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+  });
+});
