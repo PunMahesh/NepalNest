a1a328131bbf015c615d08d5242559ecb5971e13
PunMahesh <np03cs4a210055@heraldcollege.edu.np>
2024-04-03 15:21:20 +0545

    givene url to new view function

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,11 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('registration', views.registration_view, name='registration'),
     path('logout/', views.logout_view, name='logout'),
+    path('update-profile/', views.update_profile, name='update_profile'),
+    path('update-password/', views.update_password, name='update_password'),
+    path('update-profile-pic/', views.update_profile_pic, name='update_profile_pic'),
 
+    
     path('forget_Message', views.ForgetMessage, name='forget_Message'),
     path('forget-password/' , views.ForgetPassword , name="forget_password"),
     path('change-password/<token>/' , views.ChangePassword , name="change_password"),
