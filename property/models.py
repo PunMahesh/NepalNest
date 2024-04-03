551bb19bbf91104965d4d0fc8416dca97245ddad
PunMahesh <np03cs4a210055@heraldcollege.edu.np>
2024-04-03 15:27:56 +0545

    model updated property and booking

--- property/models.py
+++ property/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
 
@@ -29,6 +30,7 @@ class Extra_Items(models.Model):
 
 
 class PropertyInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     PROPERTY_TYPES = [
         ('House', 'House'),
         ('Apartment', 'Apartment'),
@@ -58,18 +60,47 @@ class PropertyInfo(models.Model):
     ammenities = models.ManyToManyField(Ammenities)
     other_ammenities = models.ManyToManyField(Other_Ammenities)
     safety_items = models.ManyToManyField(Safety_Items)
-    PropertyPhoto = models.ImageField(upload_to="Photo", blank=True)
     title = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField(default=0, null=True)
     extra_items = models.ManyToManyField(Extra_Items)
 
+class PropertyPhoto(models.Model):
+    property_info = models.ForeignKey(PropertyInfo, related_name="property_photo", on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to = "Property_Photos")
 
 
+class Review(models.Model):
+    property = models.ForeignKey('PropertyInfo', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.TextField(max_length=255, blank=True)
+    rating = models.FloatField()
+    status = models.BooleanField(default=False, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Review for {self.property} by {self.user.full_name}"
 
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(PropertyInfo, on_delete=models.CASCADE)
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    number_of_days = models.IntegerField()
+    num_guests = models.IntegerField()
+    before_service_fee = models.FloatField()
+    service_fee = models.FloatField()
+    total_price = models.FloatField()
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('declined', 'Declined'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', blank=True)
 
-
+    def __str__(self):  
+        return f"{self.property.title} - {self.check_in_date} to {self.check_out_date}"
 
 
 
